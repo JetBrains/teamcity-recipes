12f3fb3ce28f2e73761a6de5d35d90234f84e5ab
Pavel.Krizskii <pavel.krizskii@jetbrains.com>
2025-10-17 08:30:14 +0000

    TW-96570 Fix dagger test (remove stop engine logic)
    Merge-request: TC-MR-18286
    Merged-by: Pavel Krizskii <Pavel.Krizskii@jetbrains.com>

--- .teamcity/recipes/dagger.kt
+++ .teamcity/recipes/dagger.kt
@@ -24,20 +24,12 @@ object Dagger : BuildType({
             type = Recipes.Dagger
             input("version", "0.19.2")
             input("command", "dagger init --name test-module")
-            input("stop_engine", "false")
             input("installation_path", """%teamcity.agent.tools.dir%""")
         }
         script {
             id = "cmd"
             name = "Call dagger from command line step"
             scriptContent = "dagger version"
-        }
-        step {
-            id = "StopDagger"
-            name = "Stop Dagger engine"
-            type = Recipes.Dagger
-            input("version", "0.19.2")
-            input("installation_path", """%teamcity.agent.tools.dir%""")
         }
     }
 
